3c2cab255fe600f3949b6238c4315e907048cdba
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-16 18:24:42 -0500

    handlong dinamyc routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +15,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home:  HomePage() //cambgie el home de la app importando otra pagina 
+      //home:  HomePage() //cambgie el home de la app importando otra pagina 
+      initialRoute: '/',
+      routes:<String, WidgetBuilder>{
+        '/':(BuildContext context) =>HomePage(),
+        'alert':(BuildContext context) =>AlertPage(),
+        'avatar':(BuildContext context) =>AvatarPage(),
+      },
+      onGenerateRoute: (settings){ //cuando una ruta no esta definida entonces entra a ONgenerateROute y se hace alguna logica
+        print('Ruta llamada '+settings.name);
+        return MaterialPageRoute(
+          builder: (BuildContext context)=>AlertPage()
+        );
+      },
     );
   }
 }
