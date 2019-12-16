49a89732f23f7e2129512dcc4f42543a8355e088
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-16 17:35:57 -0500

    loading data options from json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,19 +14,40 @@ class HomePage extends StatelessWidget {
       
   Widget _lista() {
 
-    print(menuProvider.opciones);//need to import menu_provider
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder:(context, AsyncSnapshot<List<dynamic>> snapshot){
+        
+        print(snapshot.data);
+
+        return ListView(
+          children:_listaItems(snapshot.data),
+        );
+      },
     );
+    
+    //print(menuProvider.opciones);//need to import menu_provider
+    
   }
 
-  List<Widget> _listaItems() {
-    return[
-      ListTile(title: Text("HOliwas")),
-      Divider(),
-      ListTile(title: Text("HOliwas")),
-      Divider(),
-      ListTile(title: Text("HOliwas"))
-    ] ;
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt){
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),//va antes del texto en el list
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: (){
+        
+        },
+
+      );
+
+      opciones..add(widgetTemp)
+              ..add(Divider());
+    }); 
+
+    return opciones; 
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'dart:convert';
+
+
 class _MenuProvider{
   List<dynamic> opciones =[];
 
   _MenuProvider(){
     cargarData();
   }
-
-  cargarData() {
-    rootBundle.loadString("data/menu_opts.json").then((data){//read static resources in de directory
-      print(data);
-    });
+  //un future va a resolver va a retornar la informacion dentro de los <>
+  Future<List<dynamic>>cargarData() async{
+    final resp= await rootBundle.loadString("data/menu_opts.json");//read static resources in de directory
+      
+    Map dataMap = json.decode(resp);//to handle the data of json in a object
+    opciones = dataMap['rutas'];//accedo a ese paramtetro de l json
+  
+    return opciones;
   }
 }
 
