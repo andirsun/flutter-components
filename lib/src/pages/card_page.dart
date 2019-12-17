9854f6b6630f1f35c0fcddabf62601ce9979302a
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-16 19:00:59 -0500

    Cards, first card with cancel and accept button

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cards"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album,color: Colors.blue,),
+            title: Text("Soy el titulo de esta tarjeta"),
+            subtitle: Text("Souy ub aslkdaslkdaslkdklasjdsakjdlkasjdalsd  aslkdjkl  alksjdlk  alksdjlk jasd a "),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Cancelar"),
+                onPressed: (){},
+              ),
+              FlatButton(
+                child: Text("Ok"),
+                onPressed: (){},
+              ),
+              
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
