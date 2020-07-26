de313ece979da64d65d616885a8b73dc338f5eac
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-26 18:59:15 -0500

    chore(): improve alert visuals

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -13,28 +13,46 @@ class AlertPage extends StatelessWidget {
           color: Colors.blue,
           textColor:  Colors.white,
           shape: StadiumBorder(),
-          onPressed:()=> _mostrarAlert(context),
+          onPressed:()=> _showAlert(context),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.keyboard_return),
-        onPressed: (){
-          //Return Page to the last
-          Navigator.pop(context);
-        },
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.keyboard_return),
+      //   onPressed: (){
+      //     //Return Page to the last
+      //     Navigator.pop(context);
+      //   },
+      // ),
     );
   }
 
-  void _mostrarAlert(BuildContext context){
+  void _showAlert(context){
     showDialog(
       context: context,
       barrierDismissible: true,
       builder: (context){
         return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20)
+          ),
           title: Text("Titulo"),
-          content: Text("Contenido"),
-          
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("Este es el contenido de la alerta que deseamos mostrar"),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: ()=>Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text("Accept"),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
         );
       } 
     );
