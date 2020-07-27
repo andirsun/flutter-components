02b54963ee5e5b40329bf43ca513af5b00ee9fbc
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-26 19:33:24 -0500

    feat(Avatars) : add new avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,27 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(7),
+            child: CircleAvatar(
+              radius: 23,
+              backgroundImage: NetworkImage('https://lh3.googleusercontent.com/proxy/n7MIHF2bwZyf35AtIHaiawBpSeuNZacSyYqOUflJE_wwkSkMQ1xiQudgpUHB1CaXV1zpXMmid_jWRquY102wPvdPEDVcmlodrIDdD74y9epb2g'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/barber2.gif'),
+          image: NetworkImage('https://www.ezanime.net/wp-content/uploads/2020/06/Robert-Downey-Jr.-950x500.jpg')),
       ),
     );
   }
